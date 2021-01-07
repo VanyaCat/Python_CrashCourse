f203d42eacbeb6021e51dcba4a0ae1a9d09d310c
Seth Lilavivat <slilavivat@illumina.com>
2021-01-06 20:51:42 -0800

    Initial commit

---
+++ ch2_strings.py
@@ -0,0 +1,14 @@
+#2-3 Personal Message
+#var = "Seth"
+#message = f"Hello, {var} would you like to learn some Python today?"
+#print(message)
+
+#2-4 Name Cases
+first_name = "Seth"
+last_name = "Lilavivat"
+
+#print(first_name.lower(), last_name.lower())
+
+full_name = f"{first_name} {last_name}"
+print(full_name, "\n")
+print(full_name.lower())

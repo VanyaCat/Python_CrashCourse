fc77bdca13450373d9ebb861b66f5e0d50b3ce7b
Jonathan Morrison <jonathan.morrison@gmail.com>
2021-01-09 06:25:21 -0700

    #2 - add other ways to print the same string

--- ch2_strings.py
+++ ch2_strings.py
@@ -13,4 +13,11 @@ full_name = f"{first_name} {last_name}"
 print(full_name, "\n")
 print(full_name.lower())
 
-# What other ways can we print a string?
+# What other ways can we print the same string?
+
+# https://docs.python.org/3/library/string.html
+print(first_name + ' ' + last_name)
+print(first_name, last_name)
+print('{} {}'.format(first_name, last_name))
+print('{0} {1}'.format(first_name, last_name))
+

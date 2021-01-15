5a52678e9d36b94cd5c332b173938911f8ca3d71
Seth Lilavivat <slilavivat@illumina.com>
2021-01-14 23:00:04 -0800

    completed another list exercise

--- lists.py
+++ lists.py
@@ -1,9 +1,22 @@
 #lists
 
+
+#3-4
 guest_list = ['Jonfen', 'Momma', 'Sox']
-message = "you are invited to dinner!"
 
-#way 1
-for guest_list in guest_list:
-    print("Hi", guest_list, message)
 
+#why when I uncomment out lines 9 - 13 it doesn't work. It changes the list to a str
+#message = "you are invited to dinner!"
+
+
+#for guest_list in guest_list:
+#    print("Hi", guest_list, message)
+
+#3-5
+old_guest = 'Jonfen'
+guest_list.remove(old_guest)
+print(old_guest.title(), "can't make it.", "\n")
+new_guest = 'Maga'
+guest_list.insert(0, new_guest)
+print(new_guest.title(), "can make it instead.", "\n")
+print("The new guest list is: ", ', '.join(guest_list))

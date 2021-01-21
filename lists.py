22c40d990e83ee0b6d9e791bec1ac92758df2432
Seth Lilavivat <slilavivat@illumina.com>
2021-01-20 21:50:40 -0800

    added another list exercise

--- lists.py
+++ lists.py
@@ -20,3 +20,18 @@ new_guest = 'Maga'
 guest_list.insert(0, new_guest)
 print(new_guest.title(), "can make it instead.", "\n")
 print("The new guest list is: ", ', '.join(guest_list))
+
+#3-6
+new_guest = 'Vanya'
+guest_list.insert(2, new_guest)
+print("The new guest list is: ", ', '.join(guest_list))
+new_guest = 'Kiko'
+guest_list.append(new_guest)
+print("The new guest list is: ", ', '.join(guest_list))
+
+#3-7
+while len(guest_list) > 2:
+    guest_list.pop()
+print("The new guest list is: ", ', '.join(guest_list))
+
+#3-8
